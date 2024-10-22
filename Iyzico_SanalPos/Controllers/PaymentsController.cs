9834b52e7ec44446825eda3b40dca3b25dbc7371
İbrahim Kaymak <ibrahimkaymak817@outlook.com>
2024-10-22 18:06:56 +0300

    Add project files.

---
+++ Iyzico_SanalPos/Controllers/PaymentsController.cs
@@ -0,0 +1,124 @@
+﻿using Iyzico_SanalPos.Models;
+using Iyzipay;
+using Iyzipay.Model;
+using Iyzipay.Request;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Iyzico_SanalPos.Controllers;
+
+public class PaymentsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpGet]
+    public async Task<IActionResult> Pay()
+    {
+        Options options = new()
+        {
+            ApiKey = "sandbox-SnXN8jqHAclrSypy8psRPAeiCunW4JQj",
+            SecretKey = "sandbox-8bUMTmiMn4u67p7S2FdqPozkJh7buITJ",
+            BaseUrl = "https://sandbox-api.iyzipay.com"
+        };
+
+
+        CreatePaymentRequest request = new CreatePaymentRequest();
+        request.Locale = Locale.TR.ToString();
+        request.ConversationId = Guid.NewGuid().ToString();
+        request.Price = "1";
+        request.PaidPrice = "1.2";
+        request.Currency = Currency.TRY.ToString();
+        request.Installment = 1;
+        request.BasketId = "B67832";
+        request.PaymentChannel = PaymentChannel.WEB.ToString();
+        request.PaymentGroup = PaymentGroup.PRODUCT.ToString();
+        request.CallbackUrl = Url.Action("PayCallBack", "Payment", null, Request.Scheme); // MVC için Callback URL
+
+        PaymentCard paymentCard = new PaymentCard();
+        paymentCard.CardHolderName = "John Doe";
+        paymentCard.CardNumber = "5528790000000008";
+        paymentCard.ExpireMonth = "12";
+        paymentCard.ExpireYear = "2030";
+        paymentCard.Cvc = "123";
+        paymentCard.RegisterCard = 0;
+        request.PaymentCard = paymentCard;
+
+        Buyer buyer = new Buyer();
+        buyer.Id = "BY789";
+        buyer.Name = "John";
+        buyer.Surname = "Doe";
+        buyer.GsmNumber = "+905350000000";
+        buyer.Email = "email@email.com";
+        buyer.IdentityNumber = "74300864791";
+        buyer.LastLoginDate = "2015-10-05 12:43:35";
+        buyer.RegistrationDate = "2013-04-21 15:12:09";
+        buyer.RegistrationAddress = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
+        buyer.Ip = "85.34.78.112";
+        buyer.City = "Istanbul";
+        buyer.Country = "Turkey";
+        buyer.ZipCode = "34732";
+        request.Buyer = buyer;
+
+        Address shippingAddress = new Address();
+        shippingAddress.ContactName = "Jane Doe";
+        shippingAddress.City = "Istanbul";
+        shippingAddress.Country = "Turkey";
+        shippingAddress.Description = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
+        shippingAddress.ZipCode = "34742";
+        request.ShippingAddress = shippingAddress;
+
+        Address billingAddress = new Address();
+        billingAddress.ContactName = "Jane Doe";
+        billingAddress.City = "Istanbul";
+        billingAddress.Country = "Turkey";
+        billingAddress.Description = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
+        billingAddress.ZipCode = "34742";
+        request.BillingAddress = billingAddress;
+
+        List<BasketItem> basketItems = new List<BasketItem>();
+        BasketItem firstBasketItem = new BasketItem();
+        firstBasketItem.Id = "BI101";
+        firstBasketItem.Name = "Binocular";
+        firstBasketItem.Category1 = "Collectibles";
+        firstBasketItem.Category2 = "Accessories";
+        firstBasketItem.ItemType = BasketItemType.PHYSICAL.ToString();
+        firstBasketItem.Price = "0.3";
+        basketItems.Add(firstBasketItem);
+
+        BasketItem secondBasketItem = new BasketItem();
+        secondBasketItem.Id = "BI102";
+        secondBasketItem.Name = "Game code";
+        secondBasketItem.Category1 = "Game";
+        secondBasketItem.Category2 = "Online Game Items";
+        secondBasketItem.ItemType = BasketItemType.VIRTUAL.ToString();
+        secondBasketItem.Price = "0.5";
+        basketItems.Add(secondBasketItem);
+
+        BasketItem thirdBasketItem = new BasketItem();
+        thirdBasketItem.Id = "BI103";
+        thirdBasketItem.Name = "Usb";
+        thirdBasketItem.Category1 = "Electronics";
+        thirdBasketItem.Category2 = "Usb / Cable";
+        thirdBasketItem.ItemType = BasketItemType.PHYSICAL.ToString();
+        thirdBasketItem.Price = "0.2";
+        basketItems.Add(thirdBasketItem);
+        request.BasketItems = basketItems;
+
+        ThreedsInitialize threedsInitialize = await ThreedsInitialize.Create(request, options);
+
+        return View(new PayViewModel { HtmlContent = threedsInitialize.HtmlContent, ConversationId = request.ConversationId });
+    }
+    [HttpPost]
+    public IActionResult PayCallBack([FromForm] CallBackData request)
+    {
+        return View();
+    }
+}
+public sealed record CallBackData(
+    string status,
+    string paymentId,
+    string conversationData,
+    string mdStatus,
+    long conversationId
+    );
---
+++ Iyzico_SanalPos/Models/PayViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Iyzico_SanalPos.Models;
+
+public class PayViewModel
+{
+    public string HtmlContent { get; set; }
+    public string ConversationId { get; set; }
+}
